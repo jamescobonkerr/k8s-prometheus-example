561f9b43f8ea7f85c3e0da8053bac9c140bfd4db
jamescobonkerr <jamescobonkerr@gmail.com>
2020-04-11 14:17:03 +0100

    feat(service): add metrics handler and add slow endpoint

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main.go
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/prometheus/client_golang v1.5.1
+)
--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 }
 
+func slowHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(1 * time.Second)
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	router.Handle("/metrics", promhttp.Handler())
 	router.HandleFunc("/", homeHandler)
+	router.HandleFunc("/slow", slowHandler)
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
