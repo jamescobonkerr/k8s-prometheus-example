0acec7c13e7b98acfc5ef731f1bfb75d4a84cd57
jamescobonkerr <jamescobonkerr@gmail.com>
2020-04-11 14:28:01 +0100

    feat(service): add mux middleware

--- main.go
+++ main.go
@@ -7,8 +7,27 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "go_service_http_duration_seconds",
+		Help: "Duration of HTTP requests.",
+	}, []string{"path"})
+)
+
+func prometheusMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		route := mux.CurrentRoute(r)
+		path, _ := route.GetPathTemplate()
+		timer := prometheus.NewTimer(httpDuration.WithLabelValues(path))
+		next.ServeHTTP(w, r)
+		timer.ObserveDuration()
+	})
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
@@ -21,6 +40,7 @@ func slowHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	router.Use(prometheusMiddleware)
 	router.Handle("/metrics", promhttp.Handler())
 	router.HandleFunc("/", homeHandler)
 	router.HandleFunc("/slow", slowHandler)
